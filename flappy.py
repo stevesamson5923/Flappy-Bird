113755179827c9e9164561af4926e2d8ac2e5b88
stevesamson5923 <stevesamson5923@gmail.com>
2021-04-19 06:27:30 +0530

    third commit

--- flappy.py
+++ flappy.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 
 WIDTH = 500
@@ -27,13 +28,16 @@ class Bird:
         self.y = y
         self.width = width
         self.height = height
-        self.count = -1 
+        self.count = 0 
         self.hit = False
         self.score = 0
     def draw(self,win):
-        win.blit(bird_fly[0],(self.x,self.y))
+        win.blit(bird_fly[self.count],(self.x,self.y))
     def update(self,win): 
-        pass
+        self.count = self.count + 1
+        if self.count == 8:
+            self.count = 0
+        self.draw(win)
 
 class BG:
     def __init__(self,x,y,width,height):
@@ -62,7 +66,8 @@ class Pipes:
     def draw(self,win):
         win.blit(self.pipe,(self.x,self.y))
     def update(self,win):
-        pass
+        self.x = self.x - self.velx
+        self.draw(win)
 
 bird = Bird(int(WIDTH*0.2),int(HEIGHT*0.2),WIDTH_BIRD,HEIGHT_BIRD)
 
@@ -70,14 +75,39 @@ bg1 = BG(0,0,WIDTH,HEIGHT)
 bg2 = BG(bg1.x+WIDTH+1,0,WIDTH,HEIGHT)
 
 run = True
+
+top_list=[]
+bot_list=[]
+
+dy = random.randint(-40,180)
+toppipe = Pipes(300,-40-dy,pipes[1].get_width(),pipes[1].get_height(),2,0,1)
+botpipe = Pipes(300,-350-dy,pipes[1].get_width(),pipes[1].get_height(),2,0,0)
+
+top_list.insert(0,toppipe)
+bot_list.insert(0,botpipe)
+
 while run:
     bg1.update(win)
     bg2.update(win)
-    bird.draw(win)
+    bird.update(win)
     if bg1.x <= -WIDTH:
         bg1.x = bg2.x + 500
     if bg2.x <= -WIDTH:
         bg2.x = bg1.x + WIDTH
+    
+    dy = random.randint(-40,180)
+
+    #create new pipes
+    if len(top_list) < 3:
+        toppipe = Pipes(top_list[len(top_list)-1].x+200,-40-dy,pipes[1].get_width(),pipes[1].get_height(),2,0,1)
+        botpipe = Pipes(bot_list[len(bot_list)-1].x+200,-350-dy,pipes[0].get_width(),pipes[0].get_height(),2,0,0)
+        top_list.append(toppipe)
+        bot_list.append(botpipe)
+
+    for index,pipe in enumerate(top_list):
+        top_list[index].update(win)
+        bot_list[index].update(win)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False        
